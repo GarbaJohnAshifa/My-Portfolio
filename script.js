5445c2e863feef64a5ff55008da39654b84d7dd0
Garba John <garbajohn67@gmail.com>
2026-03-30 22:27:24 +0100

    Fix portfolio and contact sections - improve CSS, HTML, and responsive design

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-$('#menu').click(function){
-    $(this).toggleClass('fa-times');
-    $('header').toggleClass('toggle');   
-}  
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');   
+    });
 });
+
 $(window).on('scroll load', function(){
-$('#menu').click(function){
-    $(this).removeClass('fa-times');
-    $('header').toggleClass('toggle');   
+    $('#menu').click(function(){
+        $(this).removeClass('fa-times');
+        $('header').toggleClass('toggle');   
+    });
+
+    $('a[href*="#"]').on('click', function(e){
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $(this).attr('href').offset().top
+        }, 500, 'linear');
+    });
 });
-
-$('a[href*='#']').on('click', function(e)){
-    e.preventDefault();
-
-$('thml, body').animate({
-    scrollTop: $($(this)).attr('href')).offset().top
-},
-);500,
-'linear'
-}
